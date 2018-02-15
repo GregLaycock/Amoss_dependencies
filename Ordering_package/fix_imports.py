40c42f6eb6598d94253498aef3f4fb07004b8437
Greg_Laycock <gr3g.lk@gmail.com>
2018-02-15 12:59:22 +0200

    Changed fix_imports to work over multiple packages

--- Ordering_package/fix_imports.py
+++ Ordering_package/fix_imports.py
@@ -1,19 +1,26 @@
 import glob
+import pathlib
+cwd = pathlib.Path.cwd()
 
-pyfiles = glob.glob('*.py')
 
-modulenames = [f.split('.')[0] for f in pyfiles]
+s_e_dir = str(pathlib.Path(cwd,'safe_eliminations'))
+sd_t_dir = str(pathlib.Path(cwd,'sdopt_tearing'))
+dirs = [s_e_dir,sd_t_dir]
 
-# print(modulenames)
+for d in dirs:
+    files = glob.glob(d+'\*.py')
+    pyfiles = [f.split('\\')[-1] for f in files]
+    module_names = [f.split('.')[0] for f in pyfiles]
 
-for f in pyfiles:
-    contents = open(f).read()
-    for m in modulenames:
-        v1 = "import " + m
-        v2 = "from " + m
-        if v1 or v2 in contents:
-            contents = contents.replace(v1, "import ."+m)
-            contents = contents.replace(v2, "from ."+m)
-    with open('new_'+f, 'w') as outf:
-        outf.write(contents)
+    for f in files:
+
+        contents = open(f).read()
+        for m in module_names:
+            v1 = "import " + m
+            v2 = "from " + m
+            if v1 or v2 in contents:
+                contents = contents.replace(v1, "import ."+m)
+                contents = contents.replace(v2, "from ."+m)
+        # with open('new_'+f, 'w') as outf:
+        #     outf.write(contents)
 
